97f7bd87142f9f75a537ef90983454a1b09be957
Yash Jha <yjha1@ualberta.ca>
2025-08-08 19:55:26 +0545

    Feat: implemented the 4x4 microkernel

---
+++ register_level_blocking.c
@@ -0,0 +1,65 @@
+#include "include/constants.h" // constants define the N
+#include "include/utils.h" // constants define the N
+
+
+// we divide the matrix multiplication operation into blocks
+#define block_size 1024 // also refered to as b in README.m
+
+
+
+// we want the tiles to fit in shape for registers for the micro-kernel
+
+
+// hinting the compiler to inline this call
+
+#define MR 4
+#define NR 4
+#define KC 256
+
+static inline void microkernel_4x4(const int* micro_A, const int* micro_B, int* C){
+    int acc[MR][NR] = {};
+
+    for(int k = 0; k < KC; k++){
+        int b[NR];
+        for(int j = 0; j < NR; j++){
+            b[j] = micro_B[k*NR + j];
+        }
+
+        for(int i = 0; i < MR; i++){
+            const int a = micro_A[k*MR + i];
+
+            for(int j = 0; j < NR; j++){
+                acc[i][j] += a * b[j];
+            }
+        }
+    }
+
+    for(int i = 0; i < MR; i++){
+        int *c_row = C + i * N;
+        for(int j = 0; j < NR; j++){
+            c_row[j] += acc[i][j];
+        }
+    }
+}
+
+
+
+void blocked_matrix_multiply(int *A, int *B, int *C){
+    
+    int n = N / block_size; // small n (i.e. number of blocks)
+    
+    // now we iterate the blocks of these matrices
+    for(int kk = 1; kk <= n; kk++){
+    for(int ii = 1; ii <= n; ii++ ){
+        for(int jj = 1; jj <= n; jj++){
+            for(int k = (kk-1)*block_size; k < (kk*block_size); k++){
+                for(int i = (ii-1)*block_size; i < (ii*block_size); i++){
+                    for(int j = (jj-1)*block_size; j < (jj*block_size); j++){
+                            C[index(i,j)] += A[index(i, k)] * B[index(k,j)];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
